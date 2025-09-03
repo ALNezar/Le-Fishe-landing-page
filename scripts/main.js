70ca9ef57efe6c2ace3bade3f9c2c3523249128b
ALNezar <170512776+ALNezar@users.noreply.github.com>
2025-09-03 14:11:46 +0300

    Style: Updated the Footer so it deosn't leave a gab by the sides

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   if (audio) {
     audio.volume = 0.1; // Set volume to 10%
   } else {
-    console.warn("Audio element not found");
+    console.warn("le Fishe Audio element not found 😔");
   }
 });
+ const max = 57 ;
+ const actual = max - 7 ;
+ const percentage = actual / max ;
+ console.log (percentage);
